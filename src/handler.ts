80c2fd0277ef168bf1bf5397046917c0f0fb4a49
Ting Chen <ting@squareup.com>
2020-10-22 23:30:21 -0400

    WIP: trying to boot with type-graphql

--- src/handler.ts
+++ src/handler.ts
@@ -1,42 +1,88 @@
-import { ApolloServer, gql, IResolvers } from 'apollo-server-lambda';
-import 'reflect-metadata'
-import { getGreeting } from '@queries/getGreeting';
+import { ApolloServer } from 'apollo-server-lambda';
+import 'reflect-metadata';
 import * as AWS from 'aws-sdk';
+import {
+  Arg,
+  buildSchema,
+  Field, ID, Mutation, ObjectType, Query, Resolver,
+} from 'type-graphql';
 
-const typeDefs = gql`
-  type Query {
-    getGreeting(firstName: String!): String
-  }
+// const typeDefs = gql`
+//   type Query {
+//     getGreeting(firstName: String!): String
+//   }
 
-  type Mutation {
-    changeNickname(firstName: String!, nickname: String!): String
-  }
+//   type Mutation {
+//     changeNickname(firstName: String!, nickname: String!): String
+//   }
 
-  type User {
-    id: ID!
-    firstName: String!
-    email: String!
-  }
-`;
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+//   type User {
+//     id: ID!
+//     firstName: String!
+//     email: String!
+//   }
+// `;
 // const resolvers: IResolvers = {
 //   Query: {
 //     getGreeting: (),
 //   },
 // };
+@ObjectType()
+class User {
+  @Field(_type => ID)
+  id: string;
 
+  @Field()
+  firstName: string;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ event, context }) => ({
-    headers: event.headers,
-    functionName: context.functionName,
-    event,
-    context,
-    dynamoDb,
-  }),
-});
+  @Field()
+  email: string;
+}
 
-export const graphql = server.createHandler();
+@Resolver(User)
+class UserResolver {
+  private dynamoDb: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient();
+
+  @Query(_returns => String)
+  async getGreeting(@Arg('firstName') firstName: string) {
+    const result = await this.dynamoDb.get({
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: { firstName },
+    }).promise();
+    const name = result.Item.nickname ?? result.Item;
+    return `Hello, ${name}`;
+  }
+
+  @Mutation(_returns => String)
+  async changeNickname(
+    @Arg('firstName') firstName: string,
+    @Arg('nickname', { nullable: true }) nickname: string,
+  ) {
+    await this.dynamoDb.update({
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: { firstName },
+      UpdateExpression: 'SET nickname = :nickname',
+      ExpressionAttributeValues: {
+        ':nickname': nickname,
+      },
+    }).promise();
+    return nickname;
+  }
+}
+
+export const graphql = async (): Promise<ReturnType<typeof server.createHandler>> => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+    context: ({ event, context }) => ({
+      headers: event.headers,
+      functionName: context.functionName,
+      event,
+      context,
+    }),
+  });
+  return server.createHandler();
+};
